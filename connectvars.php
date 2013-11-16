17fcfcaad7379a0e0e00ae8f33d9291705120e1b
Chris RuneMaster <UACElitemarine@hotmail.com>
2013-11-16 15:25:18 -0500

    Hello it's me again. Adding a login page this go around. Here we have the connection variables stored in a separate PHP file. In addition we have the beginnings of a login page.

---
+++ connectvars.php
@@ -0,0 +1,7 @@
+<?php
+  // Define database connection constants
+define('DB_HOST','webdesign4.georgianc.on.ca');
+define('DB_USER','db200225741');
+define('DB_PASSWORD','58391');
+define('DB_NAME','db200225741');
+?>
---
+++ login.php
@@ -0,0 +1,158 @@
+<!DOCTYPE html>
+<!--[if IE 8]> 				 <html class="no-js lt-ie9" lang="en" > <![endif]-->
+<!--[if gt IE 8]><!-->
+<html class="no-js" lang="en" >
+	<!--<![endif]-->
+	<!-- Name: Chris Rooney
+	Filename: index.html
+	Website Name: Houraisan Softworks
+	Description: This is the index/home page.
+	-->
+	<head>
+		<meta charset="utf-8">
+		<meta name="viewport" content="width=device-width">
+		<title>Houraisan | Home</title>
+
+		<link rel="stylesheet" href="css/foundation.css">
+		<script src="js/detectmobilebrowser.js"></script>
+		<script src="js/vendor/custom.modernizr.js"></script>
+
+	</head>
+	<body>
+		<?php require_once(connectvars.php); 
+		
+		?>
+		<div class="row">
+			<div class="large-12 columns">
+
+				<!-- Navigation -->
+				<nav class="top-bar">
+					<ul class="title-area">
+						<!-- Title Area -->
+						<li class="name">
+							<h1><a href="index.html"> <img src="img/logo.png" alt="Houraisan Productions" > </a></h1>
+						</li>
+						<li class="toggle-topbar menu-icon">
+							<a href="#"><span>menu</span></a>
+						</li>
+					</ul>
+
+					<section class="top-bar-section">
+						<ul class="left">
+							<li>
+								<a href="index.html">Home</a>
+							</li>
+							<li>
+								<a href="About-Me.html">About Me</a>
+							</li>
+							<li>
+								<a href="Projects.html">Projects</a>
+							</li>
+							<li>
+								<a href="Services.html">Services</a>
+							</li>
+							<li>
+								<a href="https://github.com/ChrisRunemaster">Github</a>
+							</li>
+							<li>
+								<a href="Contact-Me.html">Contact Me</a>
+							</li>
+						</ul>
+
+						<!--<ul class="right">
+						<li class="search">
+						<form>
+						<input type="search">
+						</form>
+						</li>
+
+						<li class="has-button">
+						<a class="small button" href="#">Search</a>
+						</li>
+						</ul>-->
+					</section>
+				</nav>
+
+				<!-- End Navigation -->
+
+				<!-- Main Content. Log in stuff will go right here! -->
+
+				<div class="row">
+					<div class="large-8 columns">
+						<h4>This is a content section.</h4>
+						<p>
+							Bacon ipsum dolor sit amet nulla ham qui sint exercitation eiusmod commodo, chuck duis velit. Aute in reprehenderit, dolore aliqua non est magna in labore pig pork biltong. Eiusmod swine spare ribs reprehenderit culpa. Boudin aliqua adipisicing rump corned beef.
+						</p>
+						<p>
+							Bacon ipsum dolor sit amet nulla ham qui sint exercitation eiusmod commodo, chuck duis velit. Aute in reprehenderit, dolore aliqua non est magna in labore pig pork biltong. Eiusmod swine spare ribs reprehenderit culpa. Boudin aliqua adipisicing rump corned beef.
+						</p>
+
+					</div>
+
+				</div>
+
+				<!-- Footer -->
+
+				<footer class="row">
+					<div class="large-12 columns">
+						<hr>
+						<div class="row">
+							<div class="large-6 columns">
+								<p>
+									&copy; Copyright Chris Rooney 2013
+								</p>
+							</div>
+							<div class="large-6 columns">
+								<ul class="inline-list right">
+									<li>
+										<a href="#top">Back to Top</a>
+									</li>
+								</ul>
+							</div>
+						</div>
+					</div>
+				</footer>
+
+				<script>
+					document.write('<script src=' + ('__proto__' in {} ? 'js/vendor/zepto' : 'js/vendor/jquery') + '.js><\/script>');
+				</script>
+
+				<script src="js/foundation.min.js"></script>
+
+				<script src="js/foundation/foundation.js"></script>
+
+				<script src="js/foundation/foundation.interchange.js"></script>
+
+				<script src="js/foundation/foundation.abide.js"></script>
+
+				<script src="js/foundation/foundation.dropdown.js"></script>
+
+				<script src="js/foundation/foundation.placeholder.js"></script>
+
+				<script src="js/foundation/foundation.forms.js"></script>
+
+				<script src="js/foundation/foundation.alerts.js"></script>
+
+				<script src="js/foundation/foundation.magellan.js"></script>
+
+				<script src="js/foundation/foundation.reveal.js"></script>
+
+				<script src="js/foundation/foundation.tooltips.js"></script>
+
+				<script src="js/foundation/foundation.clearing.js"></script>
+
+				<script src="js/foundation/foundation.cookie.js"></script>
+
+				<script src="js/foundation/foundation.joyride.js"></script>
+
+				<script src="js/foundation/foundation.orbit.js"></script>
+
+				<script src="js/foundation/foundation.section.js"></script>
+
+				<script src="js/foundation/foundation.topbar.js"></script>
+
+				<script>
+					$(document).foundation();
+				</script>
+	</body>
+</html>
